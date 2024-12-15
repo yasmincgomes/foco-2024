612f4e76b738cdabd8097b8b55114addb0f7bc02
yasmincgomes <yasmincgomes8@gmail.com>
2024-12-15 01:16:46 -0300

    correcaojs

--- js/menuMudaCor.js
+++ js/menuMudaCor.js
@@ -1,6 +1,6 @@
 window.onscroll=function(){
      var hdrJs = document.getElementById("hdrJs");
-     if (window.pageYOffset > 50) { // Altere o valor 50 para o ponto em que o menu muda
+     if (window.pageYOffset > 50) {
          hdrJs.classList.add("scrolled");
      } else {
          hdrJs.classList.remove("scrolled");
